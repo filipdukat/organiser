e386378d8d2978d6ab518c1c918c2e8921bffa8e
Filip Dukat <filipdukat@Filips-Mac-mini.local>
2024-05-17 13:41:54 +0200

    add openapi

--- src/main/java/com/github/filipdukat/organiser/controller/ProfileController.java
+++ src/main/java/com/github/filipdukat/organiser/controller/ProfileController.java
@@ -2,6 +2,8 @@ package com.github.filipdukat.organiser.controller;
 
 import com.github.filipdukat.organiser.model.Profile;
 import com.github.filipdukat.organiser.service.ProfileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +13,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/profiles")
+@Tag(name = "Profile Controller", description = "API for managing profiles")
 public class ProfileController {
 
     @Autowired
     private ProfileService profileService;
 
     @GetMapping
+    @Operation(summary = "Get all profiles", description = "Retrieve a list of all profiles")
     public List<Profile> getAllProfiles(){
         return profileService.getAllProfiles();
     }
--- src/main/java/com/github/filipdukat/organiser/controller/TaskController.java
+++ src/main/java/com/github/filipdukat/organiser/controller/TaskController.java
@@ -3,6 +3,8 @@ package com.github.filipdukat.organiser.controller;
 import com.github.filipdukat.organiser.dto.TaskCreateDTO;
 import com.github.filipdukat.organiser.model.Task;
 import com.github.filipdukat.organiser.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,29 +14,34 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
+@Tag(name = "Task Controller", description = "API for managing tasks")
 public class TaskController {
 
     @Autowired
     private TaskService taskService;
 
     @GetMapping
+    @Operation(summary = "Get all tasks", description = "Retrieve a list of all tasks")
     public List<Task> getAllTasks(){
         return taskService.getAllTasks();
     }
 
     @PostMapping
+    @Operation(summary = "Create a new task", description = "Create a new task with title and description")
     public ResponseEntity<Task> createTask(@RequestBody TaskCreateDTO taskCreateDTO) {
         Task createdTask = taskService.createTask(taskCreateDTO);
         return new ResponseEntity<>(createdTask, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a task", description = "Delete a task by its ID")
     public ResponseEntity<Void> deleteTask(@PathVariable("id") Long id){
         taskService.deleteTaskById(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get task by ID", description = "Retrieve a task by its ID")
     public Task getTaskById(@PathVariable("id") Long id) {
         return taskService.getTaskById(id);
     }
